067b555c0c573ce8e265428aa380e0df00f7a282
mars0 <mars0>
2012-10-25 19:51:40 +0200

    some optimization in the calculus of the CP: nodes are now only computed once.

--- src/CriticalPath.java
+++ src/CriticalPath.java
@@ -16,47 +16,77 @@ public class CriticalPath {
 		return this.length;
 	}
 	
+	private void initTimes() {
+		Iterator<Activity> it = project.getActivities().iterator();
+		while(it.hasNext()) {
+			Activity a = it.next();
+			if (!"START".equals(a.getName())){
+				a.setTimeMin(-1);
+				a.setTimeMax(-1);
+			}
+		}
+	}
+	
+	private boolean minTimesDone(List<Activity> aList) {
+		Iterator<Activity> it = aList.iterator();
+		while(it.hasNext()) {
+			if (it.next().getTimeMin() < 0)
+				return false;
+		}
+		return true;
+	}
+	
+	private boolean maxTimesDone(List<Activity> aList) {
+		Iterator<Activity> it = aList.iterator();
+		while(it.hasNext()) {
+			if (it.next().getTimeMax() < 0)
+				return false;
+		}
+		return true;
+	}
+	
+	
 	private void forwardPass(Activity a) {
 		List<Activity> predecessors = a.getPredecessors();
-		List<Activity> successors = a.getSuccessors();
 		
-		//search for maximum
-		Iterator<Activity> it = predecessors.iterator();
-		int max = -1;
-		while(it.hasNext()) {
-			Activity current = it.next();
-			if ((current.getDuration()+current.getTimeMin()) > max)
-				max = current.getDuration()+current.getTimeMin();
-		}
-		// check if we found a true maximum
-		if (max > -1)
+		// check if we should compute this node
+		if (minTimesDone(predecessors) && a.getTimeMin() < 0) {
+			//search for maximum
+			Iterator<Activity> it = predecessors.iterator();
+			int max = -1;
+			while(it.hasNext()) {
+				Activity current = it.next();
+				if ((current.getDuration()+current.getTimeMin()) > max)
+					max = current.getDuration()+current.getTimeMin();
+			}
 			a.setTimeMin(max);
-		// recursive invocation for successors
-		it = successors.iterator();
-		while(it.hasNext()) {
-			forwardPass(it.next());
+			// recursive invocation for successors
+			it = a.getSuccessors().iterator();
+			while(it.hasNext()) {
+				forwardPass(it.next());
+			}
 		}
 	}
 	
 	private void backwardPass(Activity a) {
-		List<Activity> predecessors = a.getPredecessors();
 		List<Activity> successors = a.getSuccessors();
 		
-		//search for minimum;
-		Iterator<Activity> it = successors.iterator();
-		int min = Integer.MAX_VALUE;
-		while(it.hasNext()) {
-			Activity current = it.next();
-			if ((current.getTimeMax()-a.getDuration()) < min)
-				min = current.getTimeMax()-a.getDuration();
-		}
-		// check if we found a true minimum
-		if ( min < Integer.MAX_VALUE)
+		// check if we should compute this node
+		if (maxTimesDone(successors) && a.getTimeMax() < 0) {
+			//search for minimum;
+			Iterator<Activity> it = successors.iterator();
+			int min = Integer.MAX_VALUE;
+			while(it.hasNext()) {
+				Activity current = it.next();
+				if ((current.getTimeMax()-a.getDuration()) < min)
+					min = current.getTimeMax()-a.getDuration();
+			}
 			a.setTimeMax(min);
-		// recursive invocation for predecessors
-		it = predecessors.iterator();
-		while(it.hasNext()) {
-			backwardPass(it.next());
+			// recursive invocation for predecessors
+			it = a.getPredecessors().iterator();
+			while(it.hasNext()) {
+				backwardPass(it.next());
+			}
 		}
 	}
 	
@@ -110,10 +140,19 @@ public class CriticalPath {
 		Activity e = project.getActivityByName("END");
 		// check if graph is complete
 		if (s != null && e != null) {
-			// search for start activity
-			forwardPass(s);
+			// init min and max times to -1
+			initTimes(); 
+			// call forwardPass for successors of START
+			Iterator<Activity> it = s.getSuccessors().iterator();
+			while(it.hasNext()) {
+				forwardPass(it.next());
+			}
 			e.setTimeMax(e.getTimeMin());
-			backwardPass(e);
+			// call backwardPass for predecessors of END
+			it = e.getPredecessors().iterator();
+			while(it.hasNext()) {
+				backwardPass(it.next());
+			}
 			computeFloats();
 			//set length of critical path
 			this.length = project.getActivityByName("END").getTimeMax();
