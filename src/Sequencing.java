9942db88a1e100b32943757cf922906800358c88
mars0 <mars0>
2012-11-26 14:38:38 +0100

    Sequencing (serial+minLFT) should now be working.

--- src/Sequencing.java
+++ src/Sequencing.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Sequencing {
 	
-	//private int[][] availableResources;
 	private Project project;
 	private int numberOfRes;
 	
@@ -13,7 +12,6 @@ public class Sequencing {
 	}
 	
 	private Activity leastLFT(List<Activity> activities) {
-//		System.out.println("leastLFT");
 		int min = Integer.MAX_VALUE;
 		Activity res = null;
 		Iterator<Activity> it = activities.iterator();
@@ -28,7 +26,6 @@ public class Sequencing {
 	}
 	
 	private int latestFinishingTime(List<Activity> activities) {
-//		System.out.println("latestFinishingTime");
 		int time = 0;
 		Iterator<Activity> it = activities.iterator();
 		while(it.hasNext()) {
@@ -40,53 +37,52 @@ public class Sequencing {
 	}
 	
 	private int nextAvailable(Activity a, int start, Map<Integer,int[]> availableRes) {
-	//	System.out.println("nextAvailable");
 		int duration = a.getDuration();
-		int counter = 0;
 		int res[];
+		boolean success = false;
 
 		while (duration > 0) {
-			if (availableRes.get(start+counter) != null) {
-				res = availableRes.get(start+counter);
+			if (availableRes.get(start) != null) {
+				success = true;
+				res = availableRes.get(start);
 				for (int r=0; r<project.getUsedResources(); r++) {
+					//System.out.println("Available: " + res[r]);
 					if (res[r]-a.getResource(r) < 0) {
-						start += counter;
-						counter = 0;
+						//System.out.println("Confict: " + start);
 						duration = a.getDuration();
+						success = false;
 						break;
 					}
 				}
 			}
-			counter++;
-			duration--;
+			start++;
+			if (success == true) {
+				duration--;
+			}
 		}
-		return start;
+		//System.out.println(a.getName() + " starts at " + (start-a.getDuration()));
+		return (start - a.getDuration());
 	}
 	
 	private void serialize(List<Activity> e, List<Activity> c, Map<Integer,int[]> availableRes) {
 		List<Activity> elected = new ArrayList<Activity>(e);
 		List<Activity> completed = new ArrayList<Activity>(c);
     Activity current = leastLFT(elected);
-		project.getView().printDebugln("A: " + current.getName());
 		elected.remove(current);
 		int startDay = latestFinishingTime(current.getPredecessors());
+		//project.getView().printDebugln(current.getName() + " earliest start day: " + startDay);
 		startDay = nextAvailable(current, startDay, availableRes);
+		//project.getView().printDebugln(current.getName() + " start day: " + startDay);
 		current.setStartDay(startDay);
 		completed.add(current);
 
 		//update resources
-		for (int r=0; r<project.getUsedResources(); r++) {
-			if (availableRes.get(startDay) != null)
-				availableRes.get(startDay)[r] -= current.getResource(r);
-			else
-				System.out.println("Unknown start: " + startDay);
-		}
 		int[] lastUsage = new int[this.numberOfRes];
-		System.arraycopy(project.getResourceLimits(), 0, lastUsage, 0, this.numberOfRes);
+		System.arraycopy(availableRes.get(startDay), 0, lastUsage, 0, this.numberOfRes);
 		for (int i=startDay; i<startDay+current.getDuration(); i++) {
 			if (availableRes.get(i) != null) {
+				System.arraycopy(availableRes.get(i), 0, lastUsage, 0, this.numberOfRes);
 				for (int r=0; r<project.getUsedResources(); r++) {
-					System.arraycopy(availableRes.get(i), 0, lastUsage, 0, this.numberOfRes);
 					availableRes.get(i)[r] -= current.getResource(r);
 				}
 			}
@@ -118,8 +114,10 @@ public class Sequencing {
 		int resLimits[] = new int[this.numberOfRes];
 		System.arraycopy(project.getResourceLimits(), 0, resLimits, 0, this.numberOfRes);
 		availableRes.put(0, resLimits);
+		Activity start = project.getActivityByName("START");
+		start.setStartDay(0);
 		if (project.getCriticalPath().computeCriticalPath()) {
-			serialize(project.getActivityByName("START").getSuccessors(), new ArrayList<Activity>(), availableRes);
+			serialize(start.getSuccessors(), new ArrayList<Activity>(), availableRes);
 			project.calculateEndDate();
 			project.setActivityDates();
 		}
